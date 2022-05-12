1eaa942ac66c3533fd050657ab4c7237ad6e1858
Fatima_Maza <f.maza@hotmail.com>
2022-05-12 16:10:21 -0500

    update: mensajes y reinicio

--- game/game.py
+++ game/game.py
@@ -18,7 +18,6 @@ class Game: # inicializamos
         pygame.display.set_caption(TITLE)
 
         self.running = True
-        self.playing = True
 
         self.clock = pygame.time.Clock()
 
@@ -33,6 +32,7 @@ class Game: # inicializamos
     def new(self): # Genera nuevos elementos 
         self.score = 0
         self.level = 0
+        self.playing = True
 
         self.generate_elements()
         self.run()
@@ -101,6 +101,9 @@ class Game: # inicializamos
 
         if key[pygame.K_SPACE]:
             self.player.jump()
+
+        if key[pygame.K_r] and not self.playing:
+            self.new()
 
     def draw(self):
         self.surface.fill(GREY) # Asignamos color
