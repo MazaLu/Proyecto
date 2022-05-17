a313b9735d60cf425644c3b5db3e329ab70968ee
Fatima_Maza <f.maza@hotmail.com>
2022-05-17 15:04:42 -0500

    update: animaciones de jugador

--- game/player.py
+++ game/player.py
@@ -8,7 +8,13 @@ class Player (pygame.sprite.Sprite):
     def __init__(self, left, bottom, dir_images): 
         pygame.sprite.Sprite.__init__(self)
 
-        self.image = pygame.image.load(os.path.join(dir_images, "player1.png"))
+        # Animacion de cody
+        self.images = (
+            pygame.image.load(os.path.join(dir_images, "player1.png")),
+            pygame.image.load(os.path.join(dir_images, "jump.png")),
+        )
+
+        self.image = self.images[0]
 
         self.rect = self.image.get_rect()
         self.rect.left = left
@@ -34,6 +40,8 @@ class Player (pygame.sprite.Sprite):
         self.vel_y = 0
         self.can_jump = True
 
+        self.image = self.images[0]
+
     def validate_platform(self, platform):
         result = pygame.sprite.collide_rect(self, platform)
         if result:
@@ -41,10 +49,14 @@ class Player (pygame.sprite.Sprite):
             self.pos_y = platform.rect.top
             self.can_jump = True
 
+            self.image = self.images[0]
+
     def jump(self):
         if self.can_jump:
             self.vel_y = -23
             self.can_jump = False
+
+            self.image = self.images[1]
 
     def update_pos(self):
         self.vel_y += PLAYER_GRAV 
